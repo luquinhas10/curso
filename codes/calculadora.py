ccec468c610253bf32c4d64e617decbf4f4f3dca
luquinhas10 <162441126+luquinhas10@users.noreply.github.com>
2024-03-13 08:29:26 -0300

    Add files via upload

---
+++ codes/calculadora.py
@@ -0,0 +1,13 @@
+numero1=float(input("digite o primeiro número: "))
+numero2=float(input("digite o segundo número: "))
+
+
+soma=numero1+numero2
+sub=numero1-numero2
+div=numero1/numero2
+mult=numero1*numero2
+
+print("a soma dos dois numeros é ",soma)
+print("a subtração de dois numeros é ",sub)
+print("a divisão dos dois numeros é ",div)
+print("a multiplicação de dois numeros é ",mult)
