752f12b1c6534be2dfbeb7851bac3d912d567929
luquinhas10 <162441126+luquinhas10@users.noreply.github.com>
2024-03-18 11:58:31 -0300

    Add files via upload

---
+++ codes/numeroaleatorios.py
@@ -0,0 +1,8 @@
+import random
+
+num_min=int(input("DIGITE O VALOR MíNIMO: "))
+num_max=int(input("DIGITE O VALOR MÁXIMO: "))
+
+num_aleatorio=random.randint(num_min,num_max)
+
+print("O NÚMERO ALEATÓRIO E: ",num_aleatorio)
